3707cb46f6b93a4ba1fa74c2199aba443e821a93
Jady le Bars <js.lebars@gmail.com>
2024-12-02 15:01:36 +0100

    app changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
+import ColorForm from "./Components/ColorForm";
+
 import "./App.css";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+
+  // Function to handle adding a new color
+  const handleAddColor = (newColor) => {
+    setColors((prevColors) => [...prevColors, newColor]);
+  };
+
   return (
     <>
       <h1>Theme Creator</h1>
+      <ColorForm onAddColor={handleAddColor} />
 
-      {initialColors.map((color) => {
+      {colors.map((color) => {
         return <Color key={color.id} color={color} />;
       })}
     </>
