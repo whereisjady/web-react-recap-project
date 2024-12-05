9ced76f8c0cd4d4a1473f9dbf078e813fdbff510
Jady le Bars <js.lebars@gmail.com>
2024-12-05 10:54:46 +0100

    delete color done

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,43 @@ import ColorForm from "./Components/ColorForm/ColorForm";
 import { useState } from "react";
 
 function App() {
+  const [colors, setColors] = useState(initialColors);
+  const [confirmDelete, setConfirmDelete] = useState(null);
+
+  // add a color
+  function addColor(myColor) {
+    console.log(myColor);
+    setColors([myColor, ...colors]);
+  }
+
+  // delete a color (confirmation)
+  function handleDeleteConfirmation(colorId) {
+    setConfirmDelete(colorId);
+  }
+
+  // delete a color (action)
   function handleDeleteColor(colorId) {
     setColors((prevColors) =>
       prevColors.filter((color) => color.id !== colorId)
     );
+    setConfirmDelete(null);
   }
 
-  const [colors, setColors] = useState(initialColors);
-  function addColor(myColor) {
-    console.log(myColor);
-    // const newColors = [myColor, ...colors];
-    setColors([myColor, ...colors]);
-  }
-  console.log(colors);
   return (
     <>
       <h1>Theme Creator</h1>
+
       <ColorForm onAddColor={addColor} />
       {colors.map((color) => {
         return (
-          <Color key={color.id} color={color} onDelete={handleDeleteColor} />
+          <Color
+            key={color.id}
+            color={color}
+            onDelete={handleDeleteConfirmation}
+            confirmDelete={confirmDelete === color.id}
+            handleDeleteColor={handleDeleteColor}
+            setConfirmDelete={setConfirmDelete}
+          />
         );
       })}
     </>
