71d7620c4fd87036e6acb290799482a951a76774
Jady le Bars <js.lebars@gmail.com>
2024-12-05 11:49:38 +0100

    optimised delete feature

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,12 +1,9 @@
 import "./Color.css";
+import { useState } from "react";
 
-export default function Color({
-  color,
-  onDelete,
-  confirmDelete,
-  handleDeleteColor,
-  setConfirmDelete,
-}) {
+export default function Color({ color, handleDeleteColor }) {
+  const [confirmDelete, setConfirmDelete] = useState(null);
+
   return (
     <div
       className="color-card"
@@ -15,21 +12,18 @@ export default function Color({
         color: color.contrastText,
       }}
     >
-      {confirmDelete ? (
+      <h3 className="color-card-headline">{color.hex}</h3>
+      <h4>{color.role}</h4>
+      <p>contrast: {color.contrastText}</p>
+      <button onClick={() => setConfirmDelete(color.id)}>Delete</button>
+      {confirmDelete && (
         <div className="color-card-highlight">
           <p>Are you sure you want to delete this color?</p>
-          <button onClick={() => handleDeleteColor(color.id)}>
+          <button onClick={() => handleDeleteColor(confirmDelete)}>
             Yes, delete
           </button>
           <button onClick={() => setConfirmDelete(null)}>Cancel</button>
         </div>
-      ) : (
-        <>
-          <h3 className="color-card-headline">{color.hex}</h3>
-          <h4>{color.role}</h4>
-          <p>contrast: {color.contrastText}</p>
-          <button onClick={() => onDelete(color.id)}>Delete</button>
-        </>
       )}
     </div>
   );
