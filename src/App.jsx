4e09e64ede073a744590ce83bbd85e9fa0f77826
Jady le Bars <js.lebars@gmail.com>
2024-12-06 12:33:27 +0100

    local storage done

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm/ColorForm";
 import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [colors, setColors] = useState(initialColors);
+  const [colors, setColors] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
   const [editColor, setEditColor] = useState(null);
 
   // add a color
